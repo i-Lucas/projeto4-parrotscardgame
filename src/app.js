7ce8b23bfe02f6449628a2ec0eaa5a2374529468
Lucas <novo.contato.lucas@gmail.com>
2022-02-02 15:38:56 -0300

    problema das cartas

--- src/app.js
+++ src/app.js
@@ -2,51 +2,75 @@ const Verificacao = (numero) => Validar(numero) === 'valido' && ParoImpar(numero
 const Validar = (numero) => numero < 4 || numero > 14 ? 'invalido' : 'valido';
 const ParoImpar = (numero) => numero % 2 === 0 ? 'par' : 'impar';
 const Embaralhar = () => Math.random() - 0.5;
+let arrayCartas = []
 
 let condicao = false;
 
-Inicio()
+window.onload = () => {
 
-function Inicio() {
+    Inicio()
 
-    while (condicao == false) {
+    function Inicio() {
 
-        let numeroDeCartas = prompt('Com quantas cartas queres jogar ? [Min: 4 Max: 14]')
-
-        if (Verificacao(numeroDeCartas)) {
-            condicao = true;
-            MontarCartas(numeroDeCartas)
-
+        while (condicao == false) {
+    
+            let numeroDeCartas = prompt('Com quantas cartas queres jogar ? [Min: 4 Max: 14]')
+    
+            if (Verificacao(numeroDeCartas)) {
+                condicao = true;
+                MontarCartas(numeroDeCartas)
+    
+            }
+            console.log(condicao)
         }
-        console.log(condicao)
     }
+    
+    function MontarCartas(numero) {
+
+        let cores = ['Amarelo', 'Azul', 'Vermelho', 'Rosa', 'Verde', 'Laranja', 'Cinza']
+        let numeroDeCartas = numero / 2;
+    
+        for (let i = 0; i < numeroDeCartas; i++) {
+
+            arrayCartas.push(`
+            <div class="card">
+                <div class="back-face face ${cores[i]}">Verso</div>
+                <div class="front-face face" onclick="VirarCarta()">Frente</div>
+            </div> `)
+    
+            arrayCartas.push(`
+            <div class="card">
+                <div class="back-face face ${cores[i]}">Verso</div>
+                <div class="front-face face" onclick="VirarCarta()">Frente</div>
+            </div> `)
+        }
+    
+        arrayCartas.sort(Embaralhar);
+        ColocarCartas(numero)
+        console.log(arrayCartas)
+    }
+    
+    function ColocarCartas(numero) {
+    
+        const lista = document.querySelector('section')
+        for (let i = 0; i < numero; i++) {
+            lista.innerHTML += arrayCartas[i]
+        }
+    }
+
 }
 
-function MontarCartas(numero) {
+// function VirarCarta() {
+        
+//     const frontFace = document.querySelector('.card')
+//     frontFace.classList.toggle('front-face face')
+//     console.log(frontFace)
 
-    const lista = document.querySelector('section')
-    let arrayCartas = []
+//     // frontFace.classList.add('.front-face')
 
-    for (let i = 0; i < numero; i++) {
 
-        arrayCartas.push(`
-        <div class="card ${i}">
-            <div class="front-face face">Frente</div>
-            <div class="back-face face">Verso</div>
-        </div> `)
+//     // console.log('carta virada')
 
-        lista.innerHTML += arrayCartas[i]
+// }
 
-        // lista.innerHTML += `
-        // <div class="card">
-        //     <div class="front-face face">Frente</div>
-        //     <div class="back-face face">Verso</div>
-        // </div>
-        // `
-    }
-
-    arrayCartas.sort(Embaralhar);
-    console.log(arrayCartas)
-
-}
-
+// <div class="back-face face ${cores[i]}" onclick="VirarCarta()>Verso</div>
